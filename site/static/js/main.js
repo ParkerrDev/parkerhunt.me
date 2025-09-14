5ca6a81124e9a3cf1ca111f8f5d1e06e6dacf16b
Parker <ksat_nega@simplelogin.com>
2025-09-14 15:36:46 -0700

    Dynamic theme improvements

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -58,19 +58,18 @@ const root = document.documentElement;
 var theme = 0; // default white
 
 function setTheme() {
-    //console.log('Theme initiated')
-    var hour = new Date().getHours();
-    //console.log(hour)
-    if (hour > 15 && theme != 1) {
-        //console.log("Theme Dark")
+    const now = new Date();
+    const minutes = now.getHours() * 60 + now.getMinutes();
+    const shouldDark = minutes >= 15 * 60 || minutes < (4 * 60 + 30); // 3:00 PM -> 4:30 AM
+
+    if (shouldDark && theme != 1) {
         root.style.setProperty('--background', getComputedStyle(root).getPropertyValue('--dark-background').trim());
         root.style.setProperty('--text-color', getComputedStyle(root).getPropertyValue('--dark-text-color').trim());
         theme = 1;
-    } else if (theme != 0){
-        //console.log("Theme White")
+    } else if (!shouldDark && theme != 0) {
         root.style.setProperty('--background', getComputedStyle(root).getPropertyValue('--light-background').trim());
         root.style.setProperty('--text-color', getComputedStyle(root).getPropertyValue('--light-text-color').trim());
-        theme = 0
+        theme = 0;
     }
 }
 
